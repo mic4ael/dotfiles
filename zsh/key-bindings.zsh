6164752ea063149d2c821f61aa6b46e73e120925
mic4ael <crossner90@gmail.com>
2016-06-02 22:09:19 +0200

    Do not use vi keybindings in zsh

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -2,7 +2,6 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
-bindkey -v                                          # Use vi key bindings
 bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 
 # emacs style
@@ -13,4 +12,3 @@ bindkey '^e' end-of-line
 bindkey -s "^[Op" "0"
 bindkey -s "^[Ol" "."
 bindkey -s "^[OM" "^M"
-
