7b26849b85f6609ce0e438b75c3fa661d3532542
mic4ael <crossner90@gmail.com>
2017-12-30 10:00:26 +0100

    Install dotfiles using Ansible

--- install.sh
+++ install.sh
@@ -1,26 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-OS=$(uname)
-
-print_title() {
-    echo "=============== $1 ===============";
-}
-
-print_title "Initializing git submodules";
-git submodule init;
-
-print_title "Linking to ~/.dotfiles";
-cp -r . $HOME/.dotfiles;
-
-print_title "Linking ZSH configuration";
-ln -s $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc;
-
-print_title "Linking VIM configuration"
-ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc;
-
-print_title "Linking TMUX configuration";
-ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf;
-
-print_title "Linking GIT config";
-ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig;
-ln -s $HOME/.dotfiles/git/gitignore $HOME/.gitignore;
+ansible-playbook install.yml -i inventory -c local
