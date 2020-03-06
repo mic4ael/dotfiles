5330e86386e8b94af9f23171a6420ed5e8b30fa1
mic4ael <crossner90@gmail.com>
2020-03-06 13:36:50 +0100

    Add git to OSX packages

--- osx.sh
+++ osx.sh
@@ -7,7 +7,7 @@ prepare() {
 
 finish() {
   brew install adwaita-icon-theme asciidoctor automake bat boost cloc cloog cmake \
-  cocoapods ctags curl dep exercism fd fzf htop httpie kubernetes-cli macvim \
+  cocoapods ctags curl dep exercism fd fzf git htop httpie kubernetes-cli macvim \
   nasm ncdu netcat nmap node pidof pipx pstree pv pyenv reattach-to-user-namespace \
   ripgrep socat speedtest-cli ssh-copy-id tcpkali tig tldr tmux tree weechat wget zsh
 
